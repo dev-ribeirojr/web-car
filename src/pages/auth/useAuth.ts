b26a0b84aebc015a6d97cfa56fe1791cfbdd7f1d
Pablo Alves <ribeirojr1522@gmail.com>
2024-02-27 21:30:16 -0300

    feat: add logOut andd clear listCars

--- src/pages/auth/useAuth.ts
+++ src/pages/auth/useAuth.ts
@@ -1,12 +1,23 @@
 import { useState } from "react"
+import { useContexts } from "../../contexts/useContexts"
 
 export function useAuth() {
 
   const [login, setLogin] = useState<boolean>(true)
+  const { handleLogOut, clearListCars } = useContexts()
 
   function handleRegisterOrLogin() {
     setLogin(!login)
   }
 
-  return { login, handleRegisterOrLogin }
+  async function logOut() {
+    try {
+      await handleLogOut()
+      clearListCars()
+    } catch (error) {
+
+    }
+  }
+
+  return { login, handleRegisterOrLogin, logOut }
 }
